82fa95cd925b055d6d74e1269e026db62bab0b4e
andriszeile <71541542+andriszeile@users.noreply.github.com>
2025-05-08 11:06:10 +0300

    111

--- sagatave/script.js
+++ sagatave/script.js
@@ -1,4 +1,18 @@
+//1.uzd
 function ievade(){
     let v = document.getElementById("nozime").value;
     document.getElementById("izvade").innerHTML = "Tu ierakstīji "+ v;
+}
+
+//2.uzd
+function ja(){
+    document.getElementById("atb_ja").style.background="green";
+}
+function ne(){
+    document.getElementById("atb_ne").style.background="red";
+}
+
+//3.uzd
+function paslept(){
+    document.getElementById("att").style.display="none";
 }
